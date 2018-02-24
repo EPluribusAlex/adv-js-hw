398b097c67b6e782ab2a600ad2dc70fcf58f6455
AlexFP <florianprescott@googlemail.com>
2018-02-24 09:22:52 -0500

    letter constructor

--- letter.js
+++ letter.js
@@ -1,3 +1,19 @@
-const Letter = function() {
+const Letter = function(char, guessed) {
+	this.char = char;
+	this.guessed = guessed;
 
+	// returns the letter if correctly guessed, or an underscore as a placeholder
+	this.returnGuess = function() {
+		if(this.guessed) {
+			return this.char;
+		}
+		return "_";
+	};
+
+	// checks if the input character matches the letter, and if so changes its state "guessed" to true
+	this.checkChar = function(newChar) {
+		if(newChar = this.char) {
+			this.guessed = true;
+		}
+	};
 };
