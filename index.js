8d7abeacdaadb0154d7e4f6a12b8bf956c4748d4
AlexFP <florianprescott@googlemail.com>
2018-02-24 10:45:12 -0500

    more inquirer logic

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const inquirer = require("inquirer");
+const Word = require('./word.js');
 
 // randomly select word and pass into 'new Word' constructor
 const wordArr = ["testing", "working", "luck"];
+const gameWord = new Word(wordArr[Math.floor(Math.random() * wordArr.length)]);
+
+let guesses = 5;
 
 // uses inquirer to prompt user for each guess, and keeps track of guesses remaining
 inquirer.prompt([
@@ -10,5 +14,9 @@ inquirer.prompt([
 		message: "Please guess a letter."
 	}
 ]).then(function(guess) {
-
+	if(gameWord.letterArr.indexOf(guess)) {
+		gameWord.makeGuess(guess);
+		}
+	}
+	else guesses--;
 });
--- word.js
+++ word.js
@@ -19,3 +19,5 @@ const Word = function(gameWord) {
 		});
 	}
 };
+
+exports Word;
